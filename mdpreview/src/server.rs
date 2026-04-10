cc24bed0eb822b5913c7094e941d1490b6f0c37b
Deniz Isikli <s215818@dtu.dk>
2026-04-10 15:25:43 +0200

    indexing

--- mdpreview/src/server.rs
+++ mdpreview/src/server.rs
@@ -10,6 +10,24 @@ use tower_http::services::ServeDir;
 use crate::state::AppState;
 use crate::websocket::ws_handler;
 
+async fn index() -> impl IntoResponse {
+    let exe_dir = std::env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_path_buf();
+
+    let static_path = if exe_dir.ends_with("debug") || exe_dir.ends_with("release") {
+        exe_dir.join("../../static")
+    } else {
+        exe_dir.join("../mdpreview/static")
+    }
+    .canonicalize()
+    .unwrap();
+
+    Html(std::fs::read_to_string(static_path.join("index.html")).unwrap())
+}
+
 pub async fn run() {
     let (tx, _) = broadcast::channel::<String>(100);
 
@@ -32,20 +50,8 @@ pub async fn run() {
     .canonicalize()
     .unwrap();
 
-    let index_path = static_path.join("index.html");
-
-    async fn index(path: std::path::PathBuf) -> impl IntoResponse {
-        Html(std::fs::read_to_string(path).unwrap())
-    }
-
     let app = Router::new()
-        .route(
-            "/",
-            get({
-                let index_path = index_path.clone();
-                move || index(index_path.clone())
-            }),
-        )
+        .route("/", get(index))
         .route("/ws", get(ws_handler))
         .route("/update", post(update_markdown))
         .nest_service("/", ServeDir::new(static_path))
